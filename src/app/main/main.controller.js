16d7901f14d173ae3f6e266b4635d935d0e3f002
Julien Barret <ibarretj@gmail.com>
2015-12-03 11:46:22 +0100

    Refactored main controller

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,30 +9,32 @@
   function MainController(eventFetching, $scope, $log) {
     var vm = this;
 
+    $scope.addNewEvent = function(newEvent) {
+      $scope.addEvent(newEvent);
+      this.newEvent = null;
+      $scope.newEventForm.$setPristine(true);
+    };
+
+    $scope.deleteEvent = function(myEvent) {
+      $log.info('deleteEvent for ' + myEvent.name);
+      eventFetching.deleteEvent(myEvent);
+      // Todo : if Ok, splice
+      var index = $scope.events.indexOf(myEvent);
+      $scope.events.splice(index, 1);
+    };
+
+    $scope.addEvent = function(myEvent) {
+      eventFetching.addEvent(myEvent);
+      //Todo : if OK, push
+      $scope.events.push(myEvent);
+    };
+
     activate();
 
     function activate() {
       // Bind recieved events to the $scope.event variable.
       $scope.events = eventFetching.mockEvents();
-
-      $scope.addNewEvent = function(newEvent) {
-        $scope.addEvent(newEvent);
-        this.newEvent = null;
-        $scope.newEventForm.$setPristine(true);
-      };
-
-      $scope.deleteEvent = function(myEvent) {
-        $log.info('deleteEvent for ' + myEvent.name);
-        var index = $scope.events.indexOf(myEvent);
-        $scope.events.splice(index, 1);
-      };
-
-      $scope.addEvent = function(myEvent) {
-        $scope.events.push(myEvent);
-      };
-
-      eventFetching.deleteEvent = $scope.deleteEvent;
-      eventFetching.addEvent = $scope.addEvent;
     }
+
   }
 })();
